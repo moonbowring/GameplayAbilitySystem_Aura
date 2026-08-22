d15a3a408745ac5b5cdb84c85579d35e3da5e172
L <1953562425@qq.com>
2026-08-22 23:57:15 +0800

    Enemy Interface

--- Source/TopDownViewDEMO/Public/Character/AuraEnemy.h
+++ Source/TopDownViewDEMO/Public/Character/AuraEnemy.h
@@ -4,14 +4,18 @@
 
 #include "CoreMinimal.h"
 #include "Character/AuraCharacterBase.h"
+#include "Interaction/EnemyInterface.h"
 #include "AuraEnemy.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class TOPDOWNVIEWDEMO_API AAuraEnemy : public AAuraCharacterBase
+class TOPDOWNVIEWDEMO_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
 {
 	GENERATED_BODY()
 	
+public:
+	virtual void HightlightActor() override;
+	virtual void UnHighlightActor() override;
 };
