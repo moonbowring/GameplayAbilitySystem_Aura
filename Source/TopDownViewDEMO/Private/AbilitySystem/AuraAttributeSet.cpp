0f5b481066124d1d3a8637be1450be2995210896
L <1953562425@qq.com>
2026-08-28 22:34:43 +0800

    Attribute Accessors

--- Source/TopDownViewDEMO/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/TopDownViewDEMO/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -2,18 +2,19 @@
 
 
 #include "AbilitySystem/AuraAttributeSet.h"
-#include "AbilitySystemComponent.h"
 #include "Net/UnrealNetwork.h"
 
 UAuraAttributeSet::UAuraAttributeSet()
 {
-	
+	InitHealth(100.f);
+	InitMana(100.f);
 }
 
 void UAuraAttributeSet::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
 {
 	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
 	
+	//表明Health属性需要网络登记   表明  哪个类的属性      哪个属性  这个属性有什么复制条件吗  复制的时候总是使用这个属性对应的通知机制
 	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, Health, COND_None, REPNOTIFY_Always);
 	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
 	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, Mana, COND_None, REPNOTIFY_Always);
@@ -24,7 +25,8 @@ void UAuraAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) co
 {
 	//GAMEPLAYATTRIBUTE_REPNOTIFY 专门用于处理AttributeSet属性的网络复制通知
 	//告诉GAS这个Attribute刚刚发生了网络复制 需要按照GAS的属性变化机制处理这次变化
-	//也就是在告诉能力系统 我们正在同步一个值 它的值刚从服务器下来并且改变了
+	//也就是在告诉能力系统 我们正在同步一个值 它的值刚从服务器下来并且改变了 现在能力系统能记录这个变化 并记录这个旧值 以备需要滚回时使用
+	//这就是GAS作者提供的一个宏，帮助我们在“网络复制已经发生之后”，把这次属性变化正确地通知给GAS
 	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, Health, OldHealth);
 }
 
--- Source/TopDownViewDEMO/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/TopDownViewDEMO/Public/AbilitySystem/AuraAttributeSet.h
@@ -4,8 +4,14 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "AbilitySystemComponent.h"
 #include "AuraAttributeSet.generated.h"
 
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+ GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+ GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+ GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+ GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 /**
  * 
  */
@@ -15,20 +21,26 @@ class TOPDOWNVIEWDEMO_API UAuraAttributeSet : public UAttributeSet
 	GENERATED_BODY()
 public:
 	UAuraAttributeSet();
+	//告诉虚幻 这个AttributeSet里哪些属性需要真正进入网络复制系统
 	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
 	
-	//Health是一个需要网络复制Replication的属性 而且客户端收到Health更新后 要调用OnRep_Health()
+	//Health是一个需要网络复制Replication的属性 而且客户端收到Health更新后 要调用OnRep_Health()这个函数
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Health, Category = "Vital Attributes")
 	FGameplayAttributeData Health;
+	//这个宏定义了一组用于访问和初始化属性的辅助函数
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, Health);
 	
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxHealth, Category = "Vital Attributes")
 	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, MaxHealth);
 	
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Mana, Category = "Vital Attributes")
 	FGameplayAttributeData Mana;
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, Mana);
 	
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_MaxMana, Category = "Vital Attributes")
 	FGameplayAttributeData MaxMana;
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, MaxMana);
 	
 	UFUNCTION()
 	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
