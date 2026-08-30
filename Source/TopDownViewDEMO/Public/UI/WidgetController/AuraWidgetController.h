b592b2e828844a2f87b3276794dc9f299d99d4b3
L <1953562425@qq.com>
2026-08-30 10:06:37 +0800

    Globe Progress Bar

--- Source/TopDownViewDEMO/Public/UI/WidgetController/AuraWidgetController.h
+++ Source/TopDownViewDEMO/Public/UI/WidgetController/AuraWidgetController.h
@@ -8,7 +8,7 @@
 #include "AuraWidgetController.generated.h"
 
 /**
- * 
+ * 这个UObject是用来专门 整理 和 提供 UI组件Widget的
  */
 UCLASS()
 class TOPDOWNVIEWDEMO_API UAuraWidgetController : public UObject
@@ -17,14 +17,14 @@ class TOPDOWNVIEWDEMO_API UAuraWidgetController : public UObject
 	
 protected:
 	UPROPERTY(BlueprintReadOnly, Category = "WidgetController")
-	TObjectPtr<APlayerController> PlayerController;
+	TObjectPtr<APlayerController> PlayerController;//这个界面控制器知道当前玩家控制器是谁
 	
 	UPROPERTY(BlueprintReadOnly, Category = "WidgetController")
-	TObjectPtr<APlayerState> PlayerState;
+	TObjectPtr<APlayerState> PlayerState;//知道当前玩家状态是谁 玩家的长期状态
 	
 	UPROPERTY(BlueprintReadOnly, Category = "WidgetController")
-	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;//知道玩家的 能力系统组件 Ability System Component 是谁
 	
 	UPROPERTY(BlueprintReadOnly, Category = "WidgetController")
-	TObjectPtr<UAttributeSet> AttributeSet;
+	TObjectPtr<UAttributeSet> AttributeSet;//知道玩家的属性集 Attribute Set 是谁
 };
